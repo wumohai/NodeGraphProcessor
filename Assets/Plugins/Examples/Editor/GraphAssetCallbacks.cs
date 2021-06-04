5e95efad1ddf55d14d243db60f0b869824764165
wqaetly <1778139321@qq.com>
2021-06-04 11:31:52 +0800

    chorn：规整项目结构，修复NodeMenuItem的onlyCompatibleWithGraph字段程序集扫描问题

---
+++ Assets/Plugins/Examples/Editor/GraphAssetCallbacks.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using GraphProcessor;
+using UnityEditor.Callbacks;
+using System.IO;
+
+public class GraphAssetCallbacks
+{
+	[MenuItem("Assets/Create/GraphProcessor", false, 10)]
+	public static void CreateGraphPorcessor()
+	{
+		var graph = ScriptableObject.CreateInstance< BaseGraph >();
+		ProjectWindowUtil.CreateAsset(graph, "GraphProcessor.asset");
+	}
+	
+	[MenuItem("Assets/Create/GraphProcessor-ExposedPropertiesGraph", false, 10)]
+	public static void CreateGraphPorcessor_ExposedPropertiesGraph()
+	{
+		var graph = ScriptableObject.CreateInstance< ExposedPropertiesGraph >();
+		ProjectWindowUtil.CreateAsset(graph, "GraphProcessor_ExposedPropertiesGraph.asset");
+	}
+
+	[OnOpenAsset(0)]
+	public static bool OnBaseGraphOpened(int instanceID, int line)
+	{
+		var asset = EditorUtility.InstanceIDToObject(instanceID) as BaseGraph;
+
+		if (asset != null && AssetDatabase.GetAssetPath(asset).Contains("Examples"))
+		{
+			EditorWindow.GetWindow<UniversalGraphWindow>().InitializeGraph(asset as BaseGraph);
+			return true;
+		}
+		return false;
+	}
+}
